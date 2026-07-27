ec81330268664014985d52c30b9224445727a04d
Nikita Simonenko <nikitatrihomkin@gmail.com>
2026-07-27 21:34:03 +0300

    security: sanitize external HTML before rendering

--- src/entities/variables/api/variables.utils.ts
+++ src/entities/variables/api/variables.utils.ts
@@ -1,12 +1,13 @@
 import parse from "html-react-parser";
+import  DOMPurify  from "dompurify";
 import type { Variable, VariablesResponse } from "./variables.types";
 
 
 export default function normalizeVariablesList(variables: VariablesResponse): Variable[] {
-  
+
   return variables.Results.map(variable => ({
     id: variable.ID.toString(),
     label: variable.Name,
-    value: parse(variable.Description)
+    value: parse( DOMPurify.sanitize(variable.Description) )
   }))
 }
